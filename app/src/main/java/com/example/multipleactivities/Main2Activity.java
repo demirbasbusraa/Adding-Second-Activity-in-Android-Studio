f67f07d1877a5be60c08d8ec6615b7260a9d69ec
demirbasbusraa <busrademirbas1612@gmail.com>
2020-03-09 12:19:55 +0300

    In Android Studio add the second Activity page. It is using very much in social media apps or anything.Multiple-Activities-with-Transfer-of-Information

--- app/src/main/java/com/example/multipleactivities/Main2Activity.java
+++ app/src/main/java/com/example/multipleactivities/Main2Activity.java
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        //SAYFALAR ARASI BİLGİ AKTARIMI
+        textView = findViewById(R.id.textView3);
+        Intent intent=getIntent();
+        textView.setText("Hello, " + intent.getStringExtra("userInput"));
+
     }
 
     public void changeScreen(View view){
--- app/src/main/java/com/example/multipleactivities/MainActivity.java
+++ app/src/main/java/com/example/multipleactivities/MainActivity.java
@@ -3,20 +3,36 @@ package com.example.multipleactivities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    String username;
+    EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+
+        username ="";
     }
 
     public void changeActivity(View view){
+
+        username = editText.getText().toString();
         Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+
+        intent.putExtra("userInput", username); //key=userInput vaalue=username
+
+
         startActivity(intent);
+
+
     }
 
 
